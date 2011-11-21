6774ae884b7db3f0e71b9da7f2a4b39bc660031d
netpro2k <netpro2k@gmail.com>
2011-11-21 01:51:16 -0500

    ruby 1.9 doesn't like non-relative local requires

--- start.rb
+++ start.rb
@@ -2,8 +2,8 @@
 require 'plugins/thermostat/siriThermostat'
 require 'plugins/testproxy/testproxy'
 require 'plugins/eliza/eliza'
-require 'tweakSiri'
-require 'siriProxy'
+require './tweakSiri'
+require './siriProxy'
 
 #Also try Eliza -- though it should really not be run "before" anything else.
 PLUGINS = [TestProxy]
