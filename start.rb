4a0fd9516464dda78426fc3e9a7061da03418679
Noah Lackstein <noah@lackstein.com>
2011-11-20 22:51:26 -0500

    Missed another capital

--- start.rb
+++ start.rb
@@ -2,8 +2,8 @@
 require 'plugins/thermostat/siriThermostat'
 require 'plugins/testproxy/testproxy'
 require 'plugins/eliza/eliza'
-require 'tweaksiri'
-require 'siriproxy'
+require 'tweakSiri'
+require 'siriProxy'
 
 #Also try Eliza -- though it should really not be run "before" anything else.
 PLUGINS = [TestProxy]
