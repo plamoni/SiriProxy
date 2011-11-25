fbfcbb4080b941a1f998dc4941595a5809104cb6
Pete Lamonica <plamoni@gmail.com>
2011-11-25 09:25:06 -0600

    Added demo of injecting arbitrary objects from a plugin

--- plugins/siriproxy-example/lib/siriproxy-example.rb
+++ plugins/siriproxy-example/lib/siriproxy-example.rb
@@ -25,7 +25,7 @@ class SiriProxy::Plugin::Example < SiriProxy::Plugin
     set_state :some_state #set a state... this is useful when you want to change how you respond after certain conditions are met!
     say "I set the state, try saying 'confirm state change'"
     
-	request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
   
   listen_for /confirm state change/i, within_state: :some_state do #this only gets processed if you're within the :some_state state!
@@ -51,6 +51,22 @@ class SiriProxy::Plugin::Example < SiriProxy::Plugin
   #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
   listen_for /siri proxy number ([0-9,]*[0-9])/i do |number|
     say "Detected number: #{number}"
+    
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+  
+  #demonstrate injection of more complex objects without shortcut methods.
+  listen_for /test map/i do
+    add_views = SiriAddViews.new
+    add_views.make_root(last_ref_id)
+    map_snippet = SiriMapItemSnippet.new
+    map_snippet.items << SiriMapItem.new
+    utterance = SiriAssistantUtteranceView.new("Testing map injection!")
+    add_views.views << utterance
+    add_views.views << map_snippet
+    
+    #you can also do "send_object object, target: :guzzoni" in order to send an object to guzzoni
+    send_object add_views #send_object takes a hash or a SiriObject object
     
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
